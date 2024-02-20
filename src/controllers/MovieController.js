7504863cd2eb5ba411ddd5d13bc8b2a8c2c5487b
João Paulo Cassatti <joao.cassatti@adhosting.internal>
2024-02-20 11:26:04 -0300

    criando filmes no Front

--- src/controllers/MovieController.js
+++ src/controllers/MovieController.js
@@ -3,7 +3,7 @@ const knex = require("../database/knex")
 class MovieController {
     async create(request, response) {
         const { title, description, tags, rating } = request.body;
-        const { user_id } = request.params
+        const { user_id } = request.params;
 
         const [note_id] = await knex("movie_notes").insert({
             title,
